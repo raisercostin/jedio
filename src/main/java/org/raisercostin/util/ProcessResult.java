8b9b3f90d463fdf029534f3b461d6141cac6837a
raisercostin <raisercostin@gmail.com>
2019-03-09 17:01:35 +0200

    fix warnings

--- src/main/java/org/raisercostin/util/ProcessResult.java
+++ src/main/java/org/raisercostin/util/ProcessResult.java
@@ -4,15 +4,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.Reader;
-import java.io.Writer;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 import org.apache.commons.io.Charsets;
-import org.apache.commons.io.output.StringBuilderWriter;
 import org.raisercostin.jedio.DirLocation;
 
 import com.google.common.base.Joiner;
@@ -28,6 +25,8 @@ import lombok.Setter;
 @Setter(value = AccessLevel.NONE)
 public class ProcessResult {
   private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ProcessResult.class);
+  private static final int EOF = -1;
+  private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
   public final String command;
   public final String output;
   public final String error;
@@ -135,37 +134,35 @@ public class ProcessResult {
       throw new RuntimeException(e);
     }
   }
+  //
+  // private static String toString(final Reader input) throws IOException {
+  // try (final StringBuilderWriter sw = new StringBuilderWriter()) {
+  // copy(input, sw);
+  // return sw.toString();
+  // }
+  // }
 
-  private static String toString(final Reader input) throws IOException {
-    try (final StringBuilderWriter sw = new StringBuilderWriter()) {
-      copy(input, sw);
-      return sw.toString();
-    }
-  }
-
-  private static int copy(final Reader input, final Writer output) throws IOException {
-    final long count = copyLarge(input, output);
-    if (count > Integer.MAX_VALUE) {
-      return -1;
-    }
-    return (int) count;
-  }
-
-  private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
-
-  private static long copyLarge(final Reader input, final Writer output) throws IOException {
-    return copyLarge(input, output, new char[DEFAULT_BUFFER_SIZE]);
-  }
-
-  private static final int EOF = -1;
-
-  private static long copyLarge(final Reader input, final Writer output, final char[] buffer) throws IOException {
-    long count = 0;
-    int n;
-    while (EOF != (n = input.read(buffer))) {
-      output.write(buffer, 0, n);
-      count += n;
-    }
-    return count;
-  }
+  // private static int copy(final Reader input, final Writer output) throws IOException {
+  // final long count = copyLarge(input, output);
+  // if (count > Integer.MAX_VALUE) {
+  // return -1;
+  // }
+  // return (int) count;
+  // }
+  //
+  //
+  //
+  // private static long copyLarge(final Reader input, final Writer output) throws IOException {
+  // return copyLarge(input, output, new char[DEFAULT_BUFFER_SIZE]);
+  // }
+  //
+  // private static long copyLarge(final Reader input, final Writer output, final char[] buffer) throws IOException {
+  // long count = 0;
+  // int n;
+  // while (EOF != (n = input.read(buffer))) {
+  // output.write(buffer, 0, n);
+  // count += n;
+  // }
+  // return count;
+  // }
 }
