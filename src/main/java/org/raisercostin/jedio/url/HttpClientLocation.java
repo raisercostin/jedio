16b43580eb6b3231a24a4e5d95e022d8a86b715c
raisercostin <raisercostin@gmail.com>
2020-06-07 22:36:12 +0300

    fix readContent async/sync

--- src/main/java/org/raisercostin/jedio/url/HttpClientLocation.java
+++ src/main/java/org/raisercostin/jedio/url/HttpClientLocation.java
@@ -1,5 +1,6 @@
 package org.raisercostin.jedio.url;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.SocketException;
 import java.net.URL;
@@ -20,6 +21,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.http.Header;
 import org.apache.http.RequestLine;
 import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -29,6 +31,7 @@ import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpCoreContext;
 import org.jedio.Audit;
+import org.jedio.Audit.AuditException;
 import org.jedio.functions.JedioFunction;
 import org.raisercostin.jedio.MetaInfo.StreamAndMeta;
 import org.raisercostin.jedio.ReadableFileLocation;
@@ -145,41 +148,42 @@ public class HttpClientLocation extends BaseHttpLocationLike<HttpClientLocation>
     request.addHeader("Cache-Control", "no-cache");
     request.addHeader("Upgrade-Insecure-Requests", "1");
     request.addHeader("User-Agent",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36");
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36");
     request.addHeader("Accept",
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
     request.addHeader("Accept-Encoding", "gzip, deflate");
     // not needed anymore - request.addHeader("Accept-Charset", "utf-8, iso-8859-1;q=0.5");
     request.addHeader("Accept-Language", "en-US,en;q=0.9,ro;q=0.8");
     java.util.Map<String, Object> attrs = Maps.newConcurrentMap();
-    HttpClientContext context = HttpClientContext.adapt(new HttpContext() {
-      @Override
-      public void setAttribute(String id, Object obj) {
-        if (id == null || obj == null) {
-          // log.warn("cannot add attribute {}:{}", id, obj);
-        } else {
-          attrs.put(id, obj);
+    HttpClientContext context = HttpClientContext.adapt(new HttpContext()
+      {
+        @Override
+        public void setAttribute(String id, Object obj) {
+          if (id == null || obj == null) {
+            // log.warn("cannot add attribute {}:{}", id, obj);
+          } else {
+            attrs.put(id, obj);
+          }
         }
-      }
 
-      @Override
-      public Object removeAttribute(String id) {
-        return attrs.remove(id);
-      }
+        @Override
+        public Object removeAttribute(String id) {
+          return attrs.remove(id);
+        }
 
-      @Override
-      public Object getAttribute(String id) {
-        return attrs.get(id);
-      }
-    });
+        @Override
+        public Object getAttribute(String id) {
+          return attrs.get(id);
+        }
+      });
     try (CloseableHttpResponse response = client.client().execute(request, context)) {
       int code = response.getStatusLine().getStatusCode();
       String reason = response.getStatusLine().getReasonPhrase();
       InputStream in = response.getEntity().getContent();
       HttpClientLocationMetaRequest req = new HttpClientLocationMetaRequest(request.getRequestLine(),
-          request.getConfig(), request.getParams(), toHeaders(request.getAllHeaders()));
+        request.getConfig(), request.getParams(), toHeaders(request.getAllHeaders()));
       HttpClientLocationMetaResponse res = new HttpClientLocationMetaResponse(response.getStatusLine(),
-          toHeaders(response.getAllHeaders()));
+        toHeaders(response.getAllHeaders()));
       context.removeAttribute(HttpCoreContext.HTTP_RESPONSE);
       context.removeAttribute(HttpCoreContext.HTTP_REQUEST);
       context.removeAttribute(HttpCoreContext.HTTP_CONNECTION);
@@ -199,11 +203,6 @@ public class HttpClientLocation extends BaseHttpLocationLike<HttpClientLocation>
     return true;
     // HttpGet get1 = new HttpGet(url.toExternalForm());
     // CloseableHttpResponse response = client.execute(get1);
-  }
-
-  @Override
-  public String readContent(Charset charset) {
-    return readContentAsync().block();
   }
 
   public Mono<String> readContentAsyncOld() {
@@ -238,9 +237,49 @@ public class HttpClientLocation extends BaseHttpLocationLike<HttpClientLocation>
   }
 
   @Override
-  public Mono<String> readContentAsync() {
+  @SneakyThrows
+  public String readContent(Charset charset) {
+    return usingInputStreamAndMeta(false, streamAndMeta -> {
+      if (streamAndMeta.meta.httpMetaResponseStatusCodeIs200()) {
+        return IOUtils.toString(streamAndMeta.is, charset);
+      }
+      throw new AuditException("Error on calling %s - meta:%s", this, streamAndMeta.meta);
+    });
+    //return readContentOld();
+    //    //return readContentAsync().block();
+    //    return HttpUtils.getFromURL(this.toExternalForm());
+  }
+
+  private String readContentOld() throws IOException, ClientProtocolException {
     HttpGet get1 = new HttpGet(url.toExternalForm());
+    CloseableHttpResponse lastResponse = null;
+    Throwable ignoredExceptionForRetry = null;
+    for (int attempt = 1; attempt <= retries; attempt++) {
+      try (CloseableHttpResponse response = client.client().execute(get1)) {
+        int code = response.getStatusLine().getStatusCode();
+        String reason = response.getStatusLine().getReasonPhrase();
+        lastResponse = response;
+        if (code == 200) {
+          final InputStream content = response.getEntity().getContent();
+          return IOUtils.toString(content);
+        } else if (code == 502 || code == 520) {
+          log.info("Attempt {} to {} failed: {}", attempt, url, response);
+        } else {
+          throw new InvalidHttpResponse("Invalid call " + this, response, null);
+        }
+      } catch (SocketException e) {
+        ignoredExceptionForRetry = Audit.warnAndRetrhrowIfNotKnown("While reading %s", e, get1);
+      } catch (RequestAbortedException e) {
+        ignoredExceptionForRetry = Audit.warnAndRetrhrowIfNotKnown("While reading %s", e, get1);
+      }
+    }
+    throw new InvalidHttpResponse("Invalid call " + this, lastResponse, ignoredExceptionForRetry);
+  }
+
+  @Override
+  public Mono<String> readContentAsync() {
     return Mono.fromCallable(() -> {
+      return readContent(this.charset1);
       // if (enableHardAbort) {
       // scheduler.schedule(() -> {
       // // redundant null check
@@ -249,29 +288,6 @@ public class HttpClientLocation extends BaseHttpLocationLike<HttpClientLocation>
       // // }
       // }, hardTimeout, TimeUnit.SECONDS);
       // }
-
-      CloseableHttpResponse lastResponse = null;
-      Throwable ignoredExceptionForRetry = null;
-      for (int attempt = 1; attempt <= retries; attempt++) {
-        try (CloseableHttpResponse response = client.client().execute(get1)) {
-          int code = response.getStatusLine().getStatusCode();
-          String reason = response.getStatusLine().getReasonPhrase();
-          lastResponse = response;
-          if (code == 200) {
-            final InputStream content = response.getEntity().getContent();
-            return IOUtils.toString(content);
-          } else if (code == 502 || code == 520) {
-            log.info("Attempt {} to {} failed: {}", attempt, url, response);
-          } else {
-            throw new InvalidHttpResponse("Invalid call " + this, response, null);
-          }
-        } catch (SocketException e) {
-          ignoredExceptionForRetry = Audit.warnAndRetrhrowIfNotKnown("While reading %s", e, get1);
-        } catch (RequestAbortedException e) {
-          ignoredExceptionForRetry = Audit.warnAndRetrhrowIfNotKnown("While reading %s", e, get1);
-        }
-      }
-      throw new InvalidHttpResponse("Invalid call " + this, lastResponse, ignoredExceptionForRetry);
     }).checkpoint("read", true);
     // response.getEntity().
     // EntityUtils.Future
