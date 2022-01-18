bae04afa3b578f71ff6295de2d46f4cf7670b585
Costin Grigore <raisercostin@gmail.com>
2022-01-18 21:22:26 +0200

    improved

--- src/main/java/org/jedio/Audit.java
+++ src/main/java/org/jedio/Audit.java
@@ -2,6 +2,7 @@ package org.jedio;
 
 import java.io.IOException;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.util.concurrent.TimeUnit;
 
 import com.google.common.base.Preconditions;
@@ -15,7 +16,6 @@ import io.vavr.collection.Map;
 import io.vavr.collection.Vector;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.Value;
@@ -24,17 +24,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.impl.execchain.RequestAbortedException;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.jedio.feature.BooleanFeature;
+import org.jedio.feature.GenericFeature;
 
-/**
- * Responsibility: log stuff but not for so many times if is an often warn. - warn - a problem that is temporary and
- * could be recovered but is rare, normally enabled - info - some information that is rare, normally enabled (catalog
- * updates, systems starts, cleanups, etc) - debug - some information that is not rare (match updates, user/api
- * interactions)
+/** Responsibility: log stuff but not for so many times if is an often warn.
+ * - warn - a problem that is temporary and could be recovered but is rare, normally enabled
+ * - info - some information that is rare, normally enabled (catalog updates, systems starts, cleanups, etc)
+ * - debug - some information that is not rare (match updates, user/api interactions)
  *
- * Notions - rare - is not dependent on how long you run or how much processing you're doing
- */
+ *  Notions
+ *  - rare - is not dependent on how long you run or how much processing you're doing
+ *  */
 @Slf4j
 public class Audit {
+  public static final BooleanFeature debugAlwaysLog = GenericFeature.booleanFeature(
+    "debugAlwaysLog", true, "jedio.debug.alwaysLog", true);
+
   public static class AuditException extends RuntimeException {
     private static final long serialVersionUID = -1727380682618906991L;
     public final String formatter;
@@ -47,6 +52,43 @@ public class Audit {
     }
 
     public AuditException(String formatter, Object... args) {
+      super(String.format(formatter, args));
+      this.formatter = formatter;
+      this.args = args;
+    }
+  }
+
+  /**Same as AuditException only that has no special treatment and should dump stacktrace on log as any runtimeexception*/
+  public static class NonAuditException extends RuntimeException {
+    private static final long serialVersionUID = -1727380682618906991L;
+    public final String formatter;
+    public final Object[] args;
+
+    public NonAuditException(Throwable cause, String formatter, Object... args) {
+      super(String.format(formatter, args), cause);
+      this.formatter = formatter;
+      this.args = args;
+    }
+
+    public NonAuditException(String formatter, Object... args) {
+      super(String.format(formatter, args));
+      this.formatter = formatter;
+      this.args = args;
+    }
+  }
+
+  public static class BusinessAuditException extends RuntimeException {
+    private static final long serialVersionUID = -1727380682618906991L;
+    public final String formatter;
+    public final Object[] args;
+
+    public BusinessAuditException(Throwable cause, String formatter, Object... args) {
+      super(String.format(formatter, args), cause);
+      this.formatter = formatter;
+      this.args = args;
+    }
+
+    public BusinessAuditException(String formatter, Object... args) {
       super(String.format(formatter, args));
       this.formatter = formatter;
       this.args = args;
@@ -93,31 +135,56 @@ public class Audit {
   private static long DEFAULT_MAX_COUNTER = 10000000;
 
   public static void info(Throwable throwable, String format, Object... values) {
-    warn(DEFAULT_MAX_COUNTER, false, throwable, format, values);
+    report(false, DEFAULT_MAX_COUNTER, false, true, throwable, format, values);
+  }
+
+  public static void infoKnown(Throwable throwable, String format, Object... values) {
+    report(true, DEFAULT_MAX_COUNTER, false, true, throwable, format, values);
+  }
+
+  public static void debug(String format, Object... values) {
+    report(false, DEFAULT_MAX_COUNTER, false, false, null, format, values);
+  }
+
+  public static void debug(Throwable throwable, String format, Object... values) {
+    report(false, DEFAULT_MAX_COUNTER, false, false, throwable, format, values);
   }
 
   public static void info(String format, Object... values) {
-    warn(DEFAULT_MAX_COUNTER, false, null, format, values);
+    report(false, DEFAULT_MAX_COUNTER, false, true, null, format, values);
+  }
+
+  public static void infoAlways(String format, Object... values) {
+    report(false, DEFAULT_MAX_COUNTER, false, true, null, format, values);
   }
 
   public static void warn(Throwable throwable, String format, Object... values) {
-    warn(DEFAULT_MAX_COUNTER, true, throwable, format, values);
+    report(false, DEFAULT_MAX_COUNTER, true, false, throwable, format, values);
   }
 
   public static void warn(String format, Object... values) {
-    warn(DEFAULT_MAX_COUNTER, true, null, format, values);
+    report(false, DEFAULT_MAX_COUNTER, true, false, null, format, values);
   }
 
+  public static void error(Throwable throwable, String format, Object... values) {
+    report(false, DEFAULT_MAX_COUNTER, true, false, throwable, format, values);
+  }
+
+  @Deprecated
   public static Throwable warnAndRetrhrowIfNotKnown(String format, Throwable throwable, Object... values) {
-    boolean knownException = warn(DEFAULT_MAX_COUNTER, true, throwable, format, format, values);
+    boolean knownException = report(false, DEFAULT_MAX_COUNTER, true, false, throwable, format, format,
+      values);
     if (!knownException) {
       return RichThrowable.nowrap(throwable, format, values);
     }
     return throwable;
   }
 
-  /** @return knownException */
-  private static boolean warn(long maxCounter, boolean isWarn, @Nullable Throwable throwable, String formatter,
+  /**@return knownException*/
+  private static boolean report(boolean reportedAsKnwon, long maxCounter, boolean isWarn,
+      boolean isInfo,
+      Throwable throwable,
+      String formatter,
       Object... args) {
     if (throwable instanceof AuditException) {
       formatter = ((AuditException) throwable).formatter;
@@ -130,7 +197,7 @@ public class Audit {
         args == null || args.length == 0 || !Throwable.class.isInstance(args[args.length - 1]),
         "Throwable should be the first value passed. Last value was that throwable %s", (Object) args);
 
-      boolean knownException = knownThrowable(throwable, formatter);
+      boolean knownException = reportedAsKnwon || knownThrowable(throwable, formatter);
       if (throwable instanceof java.lang.AssertionError) {
         throwable = new RuntimeException(
           "Maybee!!!!!! co.paralleluniverse.fibers.SuspendExecution: Oops. Forgot to instrument a method. Run your program with -Dco.paralleluniverse.fibers.verifyInstrumentation=true to catch the culprit!",
@@ -159,9 +226,13 @@ public class Audit {
               if (log.isWarnEnabled()) {
                 log.warn(message2 + "> " + throwable.getMessage() + ". Enable debug to see the stacktrace.");
               }
-            } else {
+            } else if (isInfo) {
               if (log.isInfoEnabled()) {
                 log.info(message2 + "> " + throwable.getMessage() + ". Enable debug to see the stacktrace.");
+              }
+            } else {
+              if (log.isDebugEnabled()) {
+                log.debug(message2 + "> " + throwable.getMessage() + ". Enable debug to see the stacktrace.");
               }
             }
             log.debug(message2, throwable);
@@ -169,14 +240,20 @@ public class Audit {
             if (isWarn) {
               log.warn(message2, throwable);
             } else {
-              log.info(message2, throwable);
+              if (isInfo) {
+                log.info(message2, throwable);
+              } else {
+                log.debug(message2, throwable);
+              }
             }
           }
         } else {
           if (isWarn) {
             log.warn(message2);
-          } else {
+          } else if (isInfo) {
             log.info(message2);
+          } else {
+            log.debug(message2);
           }
         }
       }
@@ -189,6 +266,10 @@ public class Audit {
   }
 
   private static boolean shouldLog(long maxCounter, Long x) {
+    boolean forcedLog = debugAlwaysLog.isEnabled();
+    if (forcedLog) {
+      return true;
+    }
     if (x > maxCounter) {
       return false;
     }
@@ -217,7 +298,17 @@ public class Audit {
     if (throwable == null) {
       return true;
     }
+    return knownThrowableNullIsNotKnown(throwable, format, 4);
+  }
+
+  private static boolean knownThrowableNullIsNotKnown(@Nullable Throwable throwable, String format, int depth) {
+    if (throwable == null) {
+      return false;
+    }
     if (throwable instanceof AuditException) {
+      return true;
+    }
+    if (throwable instanceof BusinessAuditException) {
       return true;
     }
     if (throwable.getCause() != null) {
@@ -231,40 +322,52 @@ public class Audit {
     if (throwable.getMessage() != null && throwable.getMessage().startsWith("revobet-knwon> ")) {
       return true;
     }
-    boolean knownException = false;
-    if (throwable.getCause() != null && throwable.getCause().getMessage().startsWith("Couldn't find match ")) {
+    if (throwable.getCause() != null && throwable.getCause().getMessage() != null
+        && throwable.getCause().getMessage().startsWith("Couldn't find match ")) {
       return true;
     }
-    if (// throwable instanceof ForbiddenCallException &&
+    if (//throwable instanceof ForbiddenCallException &&
     throwable.getMessage() != null && throwable.getMessage()
       .contains(
         "Unauthorized/Forbidden call FiberUrlLocation2(useCircuitBreaker=false, url=http://inplay.goalserve.com/")) {
       return true;
     }
     if (throwable instanceof java.net.UnknownHostException && throwable.getMessage().equals("api.betsapi.com")) {
-      knownException = true;
+      return true;
     } else if (throwable instanceof IOException && throwable.getMessage().contains("Socket closed")) {
-      knownException = true;
+      return true;
     } else if (throwable instanceof java.net.ConnectException
         && throwable.getMessage().startsWith("Timeout connecting to [api.betsapi.com")) {
-      knownException = true;
+      return true;
     } else if (throwable instanceof IOException
         && throwable.getMessage().startsWith("An existing connection was forcibly closed by the remote host")
         && throwable.getCause() == null) {
-      knownException = true;
+      return true;
     } else if (throwable.getClass().toString().endsWith("HighPerfUrlLocation2$InvalidHttpResponse")) {
-      knownException = true;
+      return true;
     } else if (throwable.getClass().toString().endsWith("FiberUrlLocation2$InvalidHttpResponse")) {
-      knownException = true;
+      return true;
     } else if (throwable instanceof SocketException && throwable.getMessage().equals("Socket closed")) {
-      knownException = true;
+      return true;
+    } else if (throwable instanceof SocketTimeoutException && throwable.getMessage().equals("Read timed out")) {
+      return true;
     } else if (throwable instanceof RequestAbortedException && throwable.getMessage().equals("Request aborted")) {
-      knownException = true;
-    } else if (throwable instanceof IllegalArgumentException && throwable.getMessage() != null
+      return true;
+    } else if (throwable instanceof IllegalArgumentException
+        && throwable.getMessage() != null
         && throwable.getMessage().startsWith("Invalid over/under.")) {
-      knownException = true;
+      return true;
     }
-    return knownException;
+
+    if (throwable.getCause() != null && depth > 0) {
+      return knownThrowableNullIsNotKnown(throwable.getCause(), format, depth - 1);
+    }
+    //    if (throwable.getCause() != null) {
+    //      // cannot be known since was generated by something else, maybe a NullPointerException??? huh???
+    //      return false;
+    //    }
+
+    return false;
     // TODO: If log contains
     // is hogging the CPU or blocking a thread.
     // is blocking a thread
@@ -297,11 +400,33 @@ public class Audit {
     throw auditException(e, formatter, args);
   }
 
+  public static RuntimeException throwBusinessAuditException(String formatter, Object... args) {
+    throw businessAuditException(formatter, args);
+  }
+
+  public static RuntimeException throwBusinessAuditException(Throwable e, String formatter, Object... args) {
+    throw businessAuditException(e, formatter, args);
+  }
+
   public static RuntimeException auditException(String formatter, Object... args) {
     return new AuditException(formatter, args);
   }
 
   public static RuntimeException auditException(Throwable e, String formatter, Object... args) {
     return new AuditException(e, formatter, args);
+  }
+
+  private static RuntimeException businessAuditException(String formatter, Object[] args) {
+    return new BusinessAuditException(formatter, args);
+  }
+
+  private static RuntimeException businessAuditException(Throwable e, String formatter, Object... args) {
+    return new BusinessAuditException(e, formatter, args);
+  }
+
+  public static String resetAudit() {
+    long size = all.size();
+    all.invalidateAll();
+    return "invalidated " + size;
   }
 }
