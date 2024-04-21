3c3f8422ad72747c12e3d7188115c548c258653f
Costin Grigore <raisercostin@gmail.com>
2024-04-21 16:38:06 +0300

    upgraded

--- src/test/java/org/raisercostin/jedio/url/WebClientLocation2Test.java
+++ src/test/java/org/raisercostin/jedio/url/WebClientLocation2Test.java
@@ -1,20 +1,7 @@
 package org.raisercostin.jedio.url;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.entry;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.ser.PropertyWriter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.raisercostin.jedio.url.WebClientLocation2.RequestResponse;
@@ -62,57 +49,6 @@ class WebClientLocation2Test {
           }""");
   }
 
-  private static final String FILTER_ID = "filter properties by name";
-
-  //Custom property filter
-  static class CustomFieldHidingFilter extends SimpleBeanPropertyFilter {
-    private Set<String> fieldsToHide;
-
-    public CustomFieldHidingFilter(String... fieldsToHide) {
-      this.fieldsToHide = new HashSet<>(Arrays.asList(fieldsToHide));
-    }
-
-    @Override
-    public void serializeAsField(Object pojo, JsonGenerator jgen, SerializerProvider provider, PropertyWriter writer)
-        throws Exception {
-      if (fieldsToHide.contains(writer.getName())) {
-        jgen.writeFieldName(writer.getName() + "-ignored");
-        jgen.writeString("***");
-      } else {
-        super.serializeAsField(pojo, jgen, provider, writer);
-      }
-    }
-  }
-
-  //Mixin class
-  @JsonFilter(FILTER_ID)
-  static class PropertyFilterMixin {
-  }
-
-  //Configuration method
-  public static void configureFieldHiding(ObjectMapper mapper, String... excludedFields) {
-    mapper.setFilterProvider(new SimpleFilterProvider().addFilter(
-      FILTER_ID,
-      new CustomFieldHidingFilter(excludedFields)));
-    mapper.addMixIn(Object.class, PropertyFilterMixin.class);
-  }
-  //
-  //  public static ObjectMapper cloneMapperWithSettings(ObjectMapper original) {
-  //    ObjectMapper copy = original.copy();
-  //
-  //    // Manually copy mix-ins from the original to the new mapper
-  //    for (Map.Entry<Class<?>, Class<?>> entry : original._mixIns.entrySet()) {
-  //      copy.addMixIn(entry.getKey(), entry.getValue());
-  //    }
-  //
-  //    // Manually copy the filter provider settings
-  //    if (original.getFilterProvider() != null) {
-  //      copy.setFilterProvider(original.getFilterProvider());
-  //    }
-  //
-  //    return copy;
-  //  }
-
   @Test
   void testAddingMetadataToNonJson() {
     RequestResponse content = WebClientLocation2
@@ -120,12 +56,9 @@ class WebClientLocation2Test {
       .readCompleteContentSync(null);
     String body = content.getBody();
     String meta = content.computeMetadata();
-    configureFieldHiding(Nodes.json.mapper, "server", "date", "etag", "last-modified", "report-to",
-      "x-kong-upstream-latency", "nel", "cf-ray", "alt-svc",
-      "expires", "cross-origin-opener-policy-report-only");
     meta = Nodes.json
-      //      .excluding("date", "etag", "last-modified", "report-to", "x-kong-upstream-latency", "nel", "cf-ray", "alt-svc",
-      //        "expires", "cross-origin-opener-policy-report-only")
+      .redacting("server", "date", "etag", "last-modified", "report-to", "x-kong-upstream-latency", "nel", "cf-ray",
+        "alt-svc", "expires", "cross-origin-opener-policy-report-only")
       .prettyPrint(meta);
     assertThat(meta).isEqualTo(
       """
@@ -139,16 +72,16 @@ class WebClientLocation2Test {
               "vary" : "Accept-Encoding",
               "content-type" : "text/plain",
               "cross-origin-resource-policy" : "cross-origin",
-              "cross-origin-opener-policy-report-only-ignored" : "***",
-              "report-to-ignored" : "***",
-              "date-ignored" : "***",
-              "expires-ignored" : "***",
+              "cross-origin-opener-policy-report-only-redacted" : "***",
+              "report-to-redacted" : "***",
+              "date-redacted" : "***",
+              "expires-redacted" : "***",
               "cache-control" : "private, max-age=0",
-              "last-modified-ignored" : "***",
+              "last-modified-redacted" : "***",
               "x-content-type-options" : "nosniff",
-              "server-ignored" : "***",
+              "server-redacted" : "***",
               "x-xss-protection" : "0",
-              "alt-svc-ignored" : "***",
+              "alt-svc-redacted" : "***",
               "x-http2-stream-id" : "3",
               "transfer-encoding" : "chunked"
             },
